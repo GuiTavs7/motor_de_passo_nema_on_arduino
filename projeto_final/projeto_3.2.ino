03ceaf568b2c71e9b876151186ea5d988a878861
Guilherme Tavares Pinheiro <115669765+GuiTavs7@users.noreply.github.com>
2024-04-23 09:19:24 -0300

    Update projeto_3.2.ino

--- projeto_final/projeto_3.2.ino
+++ projeto_final/projeto_3.2.ino
@@ -1,6 +1,6 @@
 // PROJETO: PERSOL INC
 // AUTOR: GUILHERME TAVARES PINHEIRO
-// DATA: 17/04/2024
+// DATA: 23/04/2024
 // OBJETIVO: AUTOMAÇÃO DA MESA DE CORTE DE TECIDO - MOVIMENTAR MOTOR DE PASSO NEMA - ALTURAS E LARGURAS
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
